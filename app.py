90196b031ec0637471364ba73256ee4e49755357
JukFiuuTech <108091130+jukfiuune@users.noreply.github.com>
2025-10-18 15:08:40 +0300

    Fix import for weather module to use correct name

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from config import WEATHER_ENABLED, STOCKS_ENABLED, YOUTUBE_ENABLED
 import helpers
 if WEATHER_ENABLED:
     from weather.routes import *
-    from weather.weatherSence3x import *
+    from weather.weatherSenseV3 import *
 if STOCKS_ENABLED: from stocks.routes import *
 if YOUTUBE_ENABLED: from youtube.routes import *
 
